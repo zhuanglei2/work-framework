c76d79104ebc48ba2137c21e85159ad1e8c18a32
zhuanglei@wayyue.com <zhuanglei@wayyue.com>
2019-12-03 11:47:25 +0800

    工作工具和架构上的测试

---
+++ src/main/java/com/zl/WordFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.zl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zl/WordFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
